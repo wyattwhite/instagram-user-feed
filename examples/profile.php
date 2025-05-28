55769b2a641e4ff17ea3d3b5182e292a240b6589
wyattwhite <120675390+wyattwhite@users.noreply.github.com>
2025-05-27 21:12:24 -0500

    Update profile.php

--- examples/profile.php
+++ examples/profile.php
@@ -23,8 +23,8 @@ try {
     echo 'User Information : ' . "\n";
     echo '============================' . "\n";
     echo 'ID               : ' . $profile->getId() . "\n";
-    echo 'Full Name        : ' . $profile->getFullName() . "\n";
-    echo 'UserName         : ' . $profile->getUserName() . "\n";
+    echo 'Full Name       : ' . $profile->getFullName() . "\n";
+    echo 'UserName ttquia_.s       : ' . $profile->getUserName() . "\n";
     echo 'Following        : ' . $profile->getFollowing() . "\n";
     echo 'Followers        : ' . $profile->getFollowers() . "\n";
     echo 'Biography        : ' . $profile->getBiography() . "\n";
